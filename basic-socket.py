da05c1819bc4d849a1d410dceb9340c458bfa857
jegan-mj <sriramjegan1995@gmail.com>
2020-04-29 10:48:04 +0530

    Changes to be committed: 	modified:   basic-socket.py

--- basic-socket.py
+++ basic-socket.py
@@ -1,8 +1,8 @@
 import socket
 try:
     print("Domain name: " + socket.getfqdn("8.8.8.8"))
-    print("Host name to IP address: " + socket.gethostbyname("www.teezle.com"))
-    print("Extended version of it: ",socket.gethostbyname_ex("www.teezle.com"))
+    print("Host name to IP address: " + socket.gethostbyname("dns.google"))
+    print("Extended version of it: ",socket.gethostbyname_ex("dns.google"))
     print("Name of the local machine: " + socket.gethostname())
 except Exception as err:
     print("Error" + str(err))
